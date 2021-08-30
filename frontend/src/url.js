831f8c7c18ef0249728c95fd7d30e9e9dd8bbea8
Parag More <paragmore306@gmail.com>
2021-08-30 15:52:21 +0530

    Changes env urls

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,7 +1,10 @@
+
+let URL
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     // dev code
-    export const URL = "http://localhost:8080"
+     URL = "http://localhost:8080"
 } else {
     // production code
-    export const URL ="http://ec2-3-109-56-229.ap-south-1.compute.amazonaws.com"
+    URL ="http://ec2-3-109-56-229.ap-south-1.compute.amazonaws.com"
 }
+export const URL
