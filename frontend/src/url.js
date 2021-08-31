12e603de97c241ebd09b49b5228388f3395e89bf
Parag More <paragmore306@gmail.com>
2021-08-31 20:10:58 +0530

    Made UI Responsive

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,10 +1,9 @@
-
-var URL
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // dev code
-      URL = "http://localhost:8080"
+var URL;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // dev code
+  URL = "http://localhost:8080";
 } else {
-    // production code
-    URL ="http://ec2-3-109-56-229.ap-south-1.compute.amazonaws.com:8080"
+  // production code
+  URL = "http://backend:8080";
 }
-export var URL
+export var URL;
